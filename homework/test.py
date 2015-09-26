beb769939bc1486fe9de82a2af40240861090696
1200010666@pku.edu.cn <1200010666@pku.edu.cn>
2015-09-26 21:22:04 +0800

    data finish

--- homework/test.py
+++ homework/test.py
@@ -2,6 +2,7 @@ from initData import InitData
 import sklearn
 from string import atof
 from matplotlib import pyplot as plt
+from sklearn.cross_validation import train_test_split
 from sklearn import neighbors
 from sklearn import linear_model
 import numpy as np
@@ -27,13 +28,20 @@ def getErr1(trainData,trainTarget,testData,testTarget,k):
     clf=neighbors.KNeighborsClassifier(k)
     clf.fit(trainData,trainTarget)
     num=len(testTarget)
-    erro=0;
+    erro=0
+    erro1=0
+    rng=np.random.RandomState(42)
     for i in range(num):
         if testTarget[i]!= clf.predict(testData[i]):
             erro=erro+1
         else:
             continue
+    traint,testData1,traintt,testTarget1=train_test_split(np.array(trainData),np.array(trainTarget),test_size=num,random_state=rng)
+    yy_=[]
+    y_pred=clf.predict(testData1)
+    print "trainErro"+str(1-np.mean(y_pred==testTarget1))
     print float(erro)/num
+
 if __name__=='__main__':
     fn=sys.argv[1]
     trainData,trainTarget,testData,testTarget=InitData(fn)
