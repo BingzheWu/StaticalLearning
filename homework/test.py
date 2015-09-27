132df837289182635871588e3ed3b1a9a655a687
1200010666@pku.edu.cn <1200010666@pku.edu.cn>
2015-09-27 09:32:44 +0800

    sa

--- homework/test.py
+++ homework/test.py
@@ -31,19 +31,15 @@ def getErr1(trainData,trainTarget,testData,testTarget,k):
     erro=0
     erro1=0
     rng=np.random.RandomState(42)
-    for i in range(num):
-        if testTarget[i]!= clf.predict(testData[i]):
-            erro=erro+1
-        else:
-            continue
+    y_pred=clf.predict(testData)
+    print k
+    print "testErro"+str(1-np.mean(y_pred==np.array(testTarget)))
     traint,testData1,traintt,testTarget1=train_test_split(np.array(trainData),np.array(trainTarget),test_size=num,random_state=rng)
     yy_=[]
     y_pred=clf.predict(testData1)
     print "trainErro"+str(1-np.mean(y_pred==testTarget1))
-    print float(erro)/num
-
 if __name__=='__main__':
     fn=sys.argv[1]
     trainData,trainTarget,testData,testTarget=InitData(fn)
     getErr(trainData,trainTarget,testData,testTarget)
-    getErr1(trainData,trainTarget,testData,testTarget,15)
+    getErr1(trainData,trainTarget,testData,testTarget,1)
