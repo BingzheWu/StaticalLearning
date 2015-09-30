a00876ca1e47b0b303dfa828df1b42f5d4ed12b0
1200010666@pku.edu.cn <1200010666@pku.edu.cn>
2015-09-30 15:44:18 +0800

    finish

---
+++ project1/GBR.py
@@ -0,0 +1,41 @@
+import sklearn.datasets
+import sklearn.linear_model
+import numpy.random
+import numpy as np
+import numpy.linalg
+from matplotlib import pyplot as plt
+from sklearn import ensemble
+from sklearn.metrics import mean_squared_error
+from sklearn.cross_validation import train_test_split
+if __name__=="__main__":
+    boston=sklearn.datasets.load_boston()
+#Get the trainning data and test data
+    rng=np.random.RandomState(13)
+    trainFeatures,testFeatures,trainTarget,testTarget=train_test_split(boston.data,boston.target,test_size=len(boston.target)/3,random_state=rng)
+#set parameter
+    params={'n_estimators': 500,'max_depth':4,'min_samples_split':1,'learning_rate':0.01,'loss':'ls'}
+#train 
+    rg=ensemble.GradientBoostingRegressor(**params)
+#Plot feature importance 
+    rg.fit(trainFeatures,trainTarget)
+    y_pred=rg.predict(testFeatures)
+    featureImportance=rg.feature_importances_
+    X=range(len(testTarget))
+    featureImportance=100.0*(featureImportance / featureImportance.max())
+    sortedIdx=np.argsort(featureImportance)
+    pos = np.arange(sortedIdx.shape[0])+.5
+    plt.barh(pos,featureImportance[sortedIdx],align='center')
+    plt.yticks(pos,boston.feature_names[sortedIdx])
+    plt.xlabel('Relative Importance')
+    plt.title('Variable Importance')
+    plt.show()
+    print "GBR(Boston)Error:"+str(mean_squared_error(testTarget,y_pred))
+    plt.plot(range(len(testTarget)),y_pred,'r--',label='Predict Price')
+    plt.plot(X,testTarget,'g',label='True Price')
+    legend=plt.legend()
+    plt.title("GBR"+" (Boston)")
+    plt.ylabel("Price(1000 USD)")
+    plt.show()
+
+
+    
--- project1/boston.py
+++ project1/boston.py
@@ -1,27 +1,33 @@
 import sklearn.datasets
-import sklearn.linear_model
+import sklearn.linear_model as linear_model
 import numpy.random
 import numpy as np
 import numpy.linalg
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
+from sklearn import ensemble
+from sklearn.metrics import mean_squared_error
 from sklearn.cross_validation import train_test_split
 if __name__=="__main__":
     boston=sklearn.datasets.load_boston()
 #Get the trainning data and test data
-    rng=np.random.RandomState(21)
-    trainFeatures,testFeatures,trainTarget,testTarget=train_test_split(boston.data,boston.target,test_size=len(boston.target)/2,random_state=rng)
+    rng=np.random.RandomState(13)
+    trainFeatures,testFeatures,trainTarget,testTarget=train_test_split(boston.data,boston.target,test_size=len(boston.target)/3,random_state=rng)
 #Create Reg
-
-#Train
-    LR=sklearn.linear_model.LinearRegression()
-    LR.fit(trainFeatures,trainTarget)
-#Predict
-    predictTargets=LR.predict(testFeatures)
-#Evaluation
-    print"Ordinary Least Squares(Boston)Error:"+str(1-np.mean(predictTargets==testTarget))
+    RG=[
+            ("LR",linear_model.LinearRegression()),
+            ("RidgeR",linear_model.RidgeCV()),
+            ("Lasso",linear_model.LassoCV(alphas=[0.01,0.05,0.1,0.5,1.0,10.0])),
+            ("Elastic Net",linear_model.ElasticNetCV()),
+    ]
 #Draw
     X=range(len(testTarget))
-    matplotlib.pyplot.plot(range(len(testTarget)),predictTargets,'r--',label='Predict Price')
-    matplotlib.pyplot.plot(X,testTarget,'g',label='True Price')
-    legend=matplotlib.pyplot.legend()
-    matplotlib.pyplot.show()
+    for name,rg in RG:
+        rg.fit(trainFeatures,trainTarget)
+        y_pred=rg.predict(testFeatures)
+        print name+"(Boston)Error:"+str(mean_squared_error(testTarget,y_pred))
+        plt.plot(range(len(testTarget)),y_pred,'r--',label='Predict Price')
+        plt.plot(X,testTarget,'g',label='True Price')
+        legend=plt.legend()
+        plt.title(name+" (Boston)")
+        plt.ylabel("Price(1000 USD)")
+        plt.show()
